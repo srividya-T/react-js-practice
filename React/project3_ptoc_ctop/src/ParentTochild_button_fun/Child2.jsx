eb8edfadf23dfa878c4fdadc7db481e4f91ffb1e
srividya-T <vidyachowdary7@gmail.com>
2022-10-03 12:26:29 +0530

    changes

--- React/project3_ptoc_ctop/src/ParentTochild_button_fun/Child2.jsx
+++ React/project3_ptoc_ctop/src/ParentTochild_button_fun/Child2.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Child2 = ({message,person,users}) => {
+const Child2 = ({ message, person, users }) => {
   return (
     <div>
       {message && <p>Message From Parent Component : {message}</p>}
 
-      {Object.keys(person).length > 0 && <p>Person Details From Parent :{person.fname} - {person.lname}</p>}   
+      {Object.keys(person).length > 0 && (
+        <p>
+          Person Details From Parent :{person.fname} - {person.lname}
+        </p>
+      )}
 
-      {users.length >0 && 
+      {users.length > 0 && (
         <ul>
-        {users.map((user,i)=>{
-          return <li key={i}>{user}</li>
-        })}
-      </ul>
-      }      
-      
-      
+          {users.map((user, i) => {
+            return <li key={i}>{user}</li>;
+          })}
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Child2
+export default Child2;
